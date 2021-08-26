81e5163250a07ff18b94013555162dc1b02964eb
Sarah Weber <sweber25@huskers.unl.edu>
2021-08-26 18:53:20 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+*Author: Sarah Weber
+*Email: sweber25@huskers.unl.edu
+*Date: 2021/08/24
+*
+*A sinmple program that will state my name and my major
+**/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Hi, my name is Sarah Weber.\n");//prints my name
+   
+
+    return 0; //ends the program
+
+}
